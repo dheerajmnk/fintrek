d012f0f0c2ecd1095782de7003df7c4989c89a87
Dheeraj Menon <dheerajmenonk@gmail.com>
2022-07-11 23:05:05 +0530

    Implement Firebase Authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBleaBpmqw7gYyZlNpVhg0BHlJN_rtzePY",
+    authDomain: "finance-tracker-eb24f.firebaseapp.com",
+    projectId: "finance-tracker-eb24f",
+    storageBucket: "finance-tracker-eb24f.appspot.com",
+    messagingSenderId: "201470410669",
+    appId: "1:201470410669:web:61f21e9b6f67a6c00cab24"
+ }
+
+ firebase.initializeApp(firebaseConfig)
+
+ const projectFirestore = firebase.firestore()
+ const projectAuth = firebase.auth()
+
+ export { projectFirestore, projectAuth }
